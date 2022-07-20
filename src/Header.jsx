14110f66f99f9f1d8c417df6276340274058a3ef
jpnunes90 <jp-nunes@hotmail.com>
2022-07-20 16:05:19 -0300

    requisito 1 concluido

---
+++ src/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class header extends React.Component {
+  render() {
+    return (
+      <section>
+        <header>
+          <h1>Sistema Solar</h1>
+        </header>
+      </section>
+    );
+  }
+}
+
+export default header;
