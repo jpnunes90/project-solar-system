c37f2f355e56553fc4a1283061a0c0bff31104cb
jpnunes90 <jp-nunes@hotmail.com>
2022-07-20 19:10:18 -0300

    concluido requisito 6

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import Title from './Title';
 import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 
 class SolarSystem extends React.Component {
   render() {
+    const planetas = planets.map((planet, index) => (<PlanetCard
+      key={ index }
+      planetName={ planet.name }
+      planetImage={ planet.image }
+    />));
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        {planetas}
       </div>
     );
   }
